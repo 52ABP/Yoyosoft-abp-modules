61d30fba89eee6b3009c5619b86959326ef1a75c
stan e <yi.hang@live.com>
2018-09-29 17:05:44 +0800

    log4net模块添加

---
+++ src/YoYo.Castle.Log4Net/Castle/Logging/Log4Net/AbpCastleLog4NetModule.cs
@@ -0,0 +1,14 @@
+﻿using Abp;
+using Abp.Modules;
+
+namespace YoYo.Castle.Logging.Log4Net
+{
+    /// <summary>
+    /// ABP Castle Log4Net module.
+    /// </summary>
+    [DependsOn(typeof(AbpKernelModule))]
+    public class AbpCastleLog4NetModule : AbpModule
+    {
+
+    }
+}
