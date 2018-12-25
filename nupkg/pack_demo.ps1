6710ed5269cad30f67c9230ad19e3d973e6b1108
角落的白板报 <werltm@hotmail.com>
2018-12-25 16:25:20 +0800

    调整文件夹目录结构信息。

---
+++ nupkg/pack_demo.ps1
@@ -0,0 +1,81 @@
+# Paths
+$packFolder = (Get-Item -Path "./" -Verbose).FullName
+#获取到当前文件夹路径
+
+
+
+$slnPath = Join-Path $packFolder "../"
+$srcPath = Join-Path $slnPath "src"
+
+# List of projects
+$projects = (
+    "Abp",
+    "Abp.AspNetCore",
+    "Abp.AspNetCore.OData",
+    "Abp.AspNetCore.SignalR",
+    "Abp.AspNetCore.TestBase",
+    "Abp.AutoMapper",
+    "Abp.Castle.Log4Net",
+    "Abp.Dapper",
+    "Abp.EntityFramework",
+    "Abp.EntityFramework.Common",
+    "Abp.EntityFramework.GraphDiff",
+    "Abp.EntityFrameworkCore",
+    "Abp.FluentMigrator",
+    "Abp.FluentValidation",
+    "Abp.HangFire",
+    "Abp.HangFire.AspNetCore",
+    "Abp.MailKit",
+    "Abp.MemoryDb",
+    "Abp.MongoDB",
+    "Abp.NHibernate",
+    "Abp.Owin",
+    "Abp.RedisCache",
+    "Abp.RedisCache.ProtoBuf",
+    "Abp.Quartz",
+    "Abp.TestBase",
+    "Abp.Web",
+    "Abp.Web.Api",
+    "Abp.Web.Api.OData",
+    "Abp.Web.Common",
+    "Abp.Web.Mvc",
+    "Abp.Web.SignalR",
+    "Abp.Web.Resources",
+    "Abp.Zero",
+    "Abp.Zero.AspNetCore",
+    "Abp.Zero.Common",
+    "Abp.Zero.EntityFramework",
+    "Abp.Zero.EntityFrameworkCore",
+    "Abp.Zero.Ldap",
+    "Abp.Zero.NHibernate",
+    "Abp.Zero.Owin",
+    "Abp.ZeroCore",
+    "Abp.ZeroCore.EntityFramework",
+    "Abp.ZeroCore.EntityFrameworkCore",
+    "Abp.ZeroCore.IdentityServer4",
+    "Abp.ZeroCore.IdentityServer4.EntityFrameworkCore"    
+)
+
+# Rebuild solution
+Set-Location $slnPath
+& dotnet restore
+
+# Copy all nuget packages to the pack folder
+foreach ($project in $projects) {
+    
+    $projectFolder = Join-Path $srcPath $project
+
+    # Create nuget pack
+    Set-Location $projectFolder
+    Remove-Item -Recurse (Join-Path $projectFolder "bin/Release")
+    & dotnet msbuild /p:Configuration=Release /p:SourceLinkCreate=true
+    & dotnet msbuild /t:pack /p:Configuration=Release /p:SourceLinkCreate=true
+
+    # Copy nuget package
+    $projectPackPath = Join-Path $projectFolder ("/bin/Release/" + $project + ".*.nupkg")
+    Move-Item $projectPackPath $packFolder
+
+}
+
+# Go back to the pack folder
+Set-Location $packFolder
