7a15de76034c6d3cb086a751b64a3ae1b9fa0fc8
jfan liu <136590076@qq.com>
2019-01-28 10:10:10 +0800

    微信支付模块

---
+++ src/YoYo.SenparcWX.TenPay/V3/Register.cs
@@ -0,0 +1,30 @@
+﻿using Senparc.Weixin.TenPay.V3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoYo.V3
+{
+    /// <summary>
+    /// V3微信支付注册
+    /// </summary>
+    public class Register<UserKeyType, TenantKeyType>
+    {
+        /// <summary>
+        /// 手动注册微信支付（V3）
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="tenantId"></param>
+        /// <param name="tenPayV3Info">()=>new TenPayV3Info()</param>
+        /// <param name="name"></param>
+        public static void RegisterTenpayV3(UserKeyType userId, TenantKeyType tenantId, Func<TenPayV3Info> tenPayV3Info, string name)
+        {
+            RegisterV3InfoCollection<UserKeyType, TenantKeyType>.Register(userId, tenantId, tenPayV3Info().MchId);
+            TenPayV3InfoCollection.Register(tenPayV3Info(), name);
+        }
+    }
+    public class Register: Register<long, long>
+    {
+
+    }
+}
