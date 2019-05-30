b3be41b16e06e92d8457b67e579da083062c0dd8
stan e <yi.hang@live.com>
2019-05-30 14:25:02 +0800

    添加Yoyo.Abp.Wechat模块

---
+++ src/Yoyo.Abp.Wechat/YoyoAbpWechatModule.cs
@@ -0,0 +1,15 @@
+using Abp;
+using Abp.Modules;
+using System;
+
+namespace YoYo
+{
+    /// <summary>
+    /// YoYo Soft Senparc.WeiXin  Module
+    /// </summary>
+    [DependsOn(typeof(AbpKernelModule))]
+    public class YoyoAbpWechatModule : AbpModule
+    {
+
+    }
+}
