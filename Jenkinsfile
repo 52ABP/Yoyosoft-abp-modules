907248c0419e763ab362b9d0d45102d72db92a2d
桐铭 梁 <werltm@hotmail.com>
2019-10-15 16:50:04 +0800

    提交。。

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,14 +2,34 @@ pipeline {
   agent any
   stages {
     stage('Build') {
+      when {
+        branch 'master'
+      }
       steps {
         sh 'echo " Start Build...."'
+        sh './build/pack.sh'
         sh 'dotnet build'
         sh 'echo "Build Complete"'
       }
     }
+       stage('Release') {
+      when {
+        branch 'master'
+          expression {
+          ciRelease action: 'check'  
+        }
+      }
+      steps {
+          withEnv(["nugetkey=${env.NUGET_KEY}"]) {
+                    sh "chmod +x ./build/pack.sh; ./build/pack.sh"
+      }
+      }
+    }
   }
   environment {
-    nugetkey = '1'
-  }
+        NUGET_KEY     = credentials('Yoyosoft-abp-modules-nuget-key')
+    }
+    triggers {
+      githubPush()
+    }
 }
---
+++ Untitled-1.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env groovy Jenkinsfile
+library 'JenkinsSharedLibraries'
+pipeline {
+  agent any
+  stages {
+    stage('Build') {
+      when {
+        branch 'master'
+      }
+      steps {      
+      sh "echo Start Build...."
+      sh "dotnet build"        
+      sh 'echo Build Complete'
+      }
+    }
+    stage('Release') {
+      when {
+        branch 'master' // master分支执行
+        expression {
+          ciRelease action: 'check' // 指 commit中有Release就执行
+        }
+      }
+      steps {
+         withEnv(["nugetkey=${env.NUGET_KEY}"]) {
+                    sh "chmod +x ./build/pack.sh; ./build/pack.sh"
+      }
+    }
+  }
+  environment {
+    NUGET_KEY = credentials('Yoyosoft-abp-modules-nuget-key')
+   
+  }
+  triggers {
+    githubPush() //触发方式为push
+  }
+}
